e89792a99aff724523529ef9f7ad9ff8d0a711f6
nitishkumar333 <ae.nitishkumar@gmail.com>
2023-04-08 23:54:04 +0530

    Hover, fade

---
+++ app.js
@@ -0,0 +1,40 @@
+// const gridImages = document.querySelectorAll('.grid-image');
+// const threeImages = document.querySelectorAll('.coll');
+
+// gridImages.forEach(image => {
+//   gsap.set(image, { opacity: 0 });
+
+//   gsap.to(image, {
+//     opacity: 1,
+//     duration: 1,
+//     scrollTrigger: {
+//       trigger: image,
+//       start: 'top 90%',
+//       end: 'bottom 60%',
+//       scrub: true,
+//     }
+//   });
+// });
+
+
+// threeImages.forEach(image => {
+//   gsap.set(image, { opacity: 0 });
+
+//   gsap.to(image, {
+//     opacity: 1,
+//     duration: 1,
+//     scrollTrigger: {
+//       trigger: image,
+//       start: 'top 90%',
+//       end: 'bottom 60%', 
+//       scrub: true,
+//     }
+//   });
+// });
+
+// const link = `https://wa.me/918800323538?text=${encodeURIComponent("https://i.imgur.com/Ha8Ls82_d.webp?maxwidth=760&fidelity=grand")}`;
+// console.log(link);
+// let fakeLink = document.createElement('a');
+// fakeLink.setAttribute('href',link);
+// fakeLink.setAttribute('data-action','share/whatsapp/share');
+// fakeLink.click();
